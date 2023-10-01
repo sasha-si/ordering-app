231f243f47dfd8d6eb1049157e7d74fc13fd8538
sasha-si <sasha.siundiukov@gmail.com>
2023-10-01 23:35:00 +0300

    Handling forms' inputs for Auth page

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
+  isSignedUpMode = true;
+  form!: FormGroup;
 
+  constructor(private fb: FormBuilder) {};
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      'email': ['', [Validators.required, Validators.email]],
+      'password': ['', [Validators.required, Validators.minLength(6)]],
+  })
+  }
+
+  onSwitchMode() {
+    this.isSignedUpMode = !this.isSignedUpMode;
+  };
+
+  onSubmit() {
+    console.log(this.form.value);
+    this.form.reset();
+  };
 }
