2501f92513a59bb05beb522c7c6e00ffcb395da7
sasha-si <sasha.siundiukov@gmail.com>
2023-10-03 23:34:32 +0300

    Sending the Signup Request

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { AuthService } from './auth.service';
+
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
@@ -10,13 +12,13 @@ export class AuthComponent implements OnInit {
   isSignedUpMode = true;
   form!: FormGroup;
 
-  constructor(private fb: FormBuilder) {};
+  constructor(private fb: FormBuilder, private auth: AuthService) { };
 
   ngOnInit(): void {
     this.form = this.fb.group({
       'email': ['', [Validators.required, Validators.email]],
       'password': ['', [Validators.required, Validators.minLength(6)]],
-  })
+    })
   }
 
   onSwitchMode() {
@@ -24,7 +26,25 @@ export class AuthComponent implements OnInit {
   };
 
   onSubmit() {
-    console.log(this.form.value);
+    if (!this.form.valid) {
+      return;
+    }
+    const { email, password } = this.form.value
+
+    if (this.isSignedUpMode) {
+      //...
+    } else {
+      this.auth.signup(email, password).subscribe(
+        {
+          next: (data) => {
+            console.log(data);
+          },
+          error: (err) => {
+            console.log(err);
+          }
+        }
+      )
+    }
     this.form.reset();
   };
 }
